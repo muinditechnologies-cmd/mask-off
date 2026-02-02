807bf6056b06b5a25e404521b686fe5404edfc07
muinditechnologies-cmd <muinditechnologies@gmail.com>
2026-02-02 17:23:35 +0000

    Describe what you changed

---
+++ script.js
@@ -0,0 +1,16 @@
+window.nucleusLauncherSettings = {
+  launcherId: '4dae84425a61dedf3a58',
+};
+(function (l, a, u, n, c, h) {
+  u = a.createElement('script');
+  u.type = 'text/javascript';
+  u.async = true;
+  u.src = 'https://launcher.nucleus.church/boot.latest.js';
+  n = a.getElementsByTagName('script')[0];
+  n.parentNode.insertBefore(u, n);
+})(window, document);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-21RP8LMGHW');
